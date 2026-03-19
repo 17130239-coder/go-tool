44711ba5fc1f1ebc9963ae11d75025463026fc39
Andy Nguyen <andynguyen@Andys-MacBook-Pro.local>
2026-03-19 17:30:40 +0700

    fix(menu): remove unused icons

--- src/constants/menuConfig.tsx
+++ src/constants/menuConfig.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {
-  DashboardOutlined,
   ToolOutlined,
-  SettingOutlined,
-  AppstoreOutlined,
 } from '@ant-design/icons';
 
 export interface MenuItemConfig {
